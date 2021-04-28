c9575c988382354307878503e8636d89b646f479
EgorPashkevich <Pashkevichegor@mail.ru>
2021-04-29 01:56:35 +0300

    3s commit

---
+++ pythontutorRU/lesson1_input&print/zad6 Next and Previous.py
@@ -0,0 +1,3 @@
+a = int(input('enter number: '))
+print('The next number for the number ', a, 'is ', a+1, '.')
+print('The previous number for the number ', a, 'is ', a-1, '.')
---
+++ pythontutorRU/lesson1_input&print/zad8 Laces.py
@@ -0,0 +1,5 @@
+a = int(input())
+b = int(input())
+l = int(input())
+N = int(input())
+print(((((N - 1) * (a + b)) + l) * 2) + a)
