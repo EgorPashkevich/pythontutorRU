5fd0fec4d12e29712561a589a7a1ca3fd1d7c4ec
EgorPashkevich <Pashkevichegor@mail.ru>
2021-06-07 01:10:35 +0300

    '.2'

--- game_cube_random/main.py
+++ game_cube_random/main.py
@@ -8,17 +8,37 @@ class Game:
         self.glass = int(input('number of glasses: '))
         self.random_dict = {'glass number:' + str(i + 1): random.randint(1, 6) for i in range(self.glass)}
         self.list_glass = [i for i in self.random_dict]
-        self.dict_players_point = {i: 0 for i in self.name_players}
+        # self.dict_players_point = {i: 0 for i in self.name_players}
+
+    def check_for_bot(self):
+        count = 0
+        index = 0
+        for i in self.name_players:
+            if i == 'bot':
+                count += 1
+        if count > 1:
+            for i in self.name_players:
+                index += 1   # это не верно, создает лишнего бота, но код работает! не могу понять причину
+                if i == 'bot':
+                    self.name_players.insert(index, 'bot №' + str(count))
+                    count -= 1
+                # index += 1    # так по логике праивльно, но не работает
 
     def assigning_points_to_player(self):
         print('Select a glass: ' + ' | '.join(self.list_glass))
         glass = input('choose: ')
-        if glass not in self.list_glass:
-            print('!!! ERROR, choose from the list !!!')
-            self.assigning_points_to_player()
-        else:
+        try:                                #  если ввести неверное значение, он повторно попросит ввести! но если уже ввел правильно, делает ошибку
             self.list_glass.remove(glass)
             return self.random_dict[glass]
+        except ValueError:
+            print('!!! ERROR, choose from the list !!!')
+            self.assigning_points_to_player()
+        # if glass not in self.list_glass:
+        #     print('!!! ERROR, choose from the list !!!')
+        #     self.assigning_points_to_player()
+        # else:
+        #     self.list_glass.remove(glass)
+        #     return self.random_dict[glass]
 
     def assigning_points_to_bot(self):
         print('Select a glass: ' + ' | '.join(self.list_glass))
@@ -44,10 +64,12 @@ class Game:
     def start(self):
         # self.assigning_names_is_classes()
         global winner
+        self.check_for_bot()
         self.sorted_name_players()
+        self.dict_players_point = {i: 0 for i in self.name_players}
         while bool(self.list_glass):
             for i in self.name_players:
-                if i == 'bot':
+                if i[:3] == 'bot':
                     print('Play ' + i)
                     self.dict_players_point[i] += self.assigning_points_to_bot()
                     print(self.dict_players_point)
